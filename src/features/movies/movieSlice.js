1e85dacbbed26da07cb2818be513ee643bda59ab
belaymit <2belamit@gmail.com>
2023-01-14 15:01:05 +0300

    used redux-thank and rewrite api-fetcher function

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import movieApi from '../../common/apis/movieApis';
+import { ApiKey } from '../../common/apis/MovieApiKeys';
+
+export const fetchAsyncMovies = createAsyncThunk(
+  'movies/fetchAsyncMovies',
+  async () => {
+    const movieText = 'Harry';
+    const response = await movieApi.get(
+      `?apiKey=${ApiKey}&s=${movieText}&type=movie`,
+    );
+    return response.data;
+  },
+);
 
 const initialState = {
   movies: {},
@@ -11,6 +24,18 @@ const movieSlice = createSlice({
     addMovies: (state, { payload }) => {
       // eslint-disable-next-line no-param-reassign
       state.movies = payload;
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log('Pending');
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log('Fetched Successfully');
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log('Rejected');
     },
   },
 });
