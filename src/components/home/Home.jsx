16c382815be0df64676947a154ba8f014510cadf
belaymit <2belamit@gmail.com>
2023-01-14 15:00:23 +0300

    removed api fetcher function from main component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,27 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import movieApis from '../../common/apis/movieApis';
 import MovieListing from '../movieListing/MovieListing';
-import { ApiKey } from '../../common/apis/MovieApiKeys';
+import { fetchAsyncMovies } from '../../features/movies/movieSlice';
 import './home.scss';
-import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const movieText = 'Harry';
-  const fetchMovies = async () => {
-    const response = await movieApis.get(
-      `?apiKey=${ApiKey}&s=${movieText}&type=movie`,
-    )
-      .catch((err) => {
-        throw (err);
-      });
-    dispatch(addMovies(response.data));
-  };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]);
 
   return (
     <div>
